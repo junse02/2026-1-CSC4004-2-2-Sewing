8947af58b18662cc56fa47eb7b74dac5688c3620
junse02 <67049713+junseo021014@users.noreply.github.com>
2026-04-05 14:16:15 +0900

    📝feat: 회원가입 기능 구현 및 PostgreSQL 연동 테스트 완료

--- src/test/java/com/mediator/cider/domain/user/service/UserServiceTest.java
+++ src/test/java/com/mediator/cider/domain/user/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import com.mediator.cider.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
