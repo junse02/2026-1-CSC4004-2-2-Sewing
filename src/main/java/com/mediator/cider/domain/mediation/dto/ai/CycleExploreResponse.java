67df4eb7285464a7666e5d801337227c8a9e645c
junse02 <67049713+junseo021014@users.noreply.github.com>
2026-05-26 00:33:10 +0900

    🔨 fix: 변수명 수정

--- src/main/java/com/mediator/cider/domain/mediation/dto/ai/CycleExploreResponse.java
+++ src/main/java/com/mediator/cider/domain/mediation/dto/ai/CycleExploreResponse.java
@@ -1,7 +1,6 @@
 package com.mediator.cider.domain.mediation.dto.ai;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,9 +8,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CycleExploreResponse {
+
+    @JsonProperty("session_id")
     private Long sessionId;
+
+    // AI 서버로부터 읽을 때는 "f_question" 키를 사용 (역직렬화)
+    // FE로 보낼 때는 "f_question"을 무시하고 자바 변수명을 사용하여 "fQuestion"으로 응답 (직렬화)
+    @JsonProperty(value = "f_question", access = JsonProperty.Access.WRITE_ONLY)
     private String fQuestion;
+
+    @JsonProperty(value = "m_question", access = JsonProperty.Access.WRITE_ONLY)
     private String mQuestion;
 }
