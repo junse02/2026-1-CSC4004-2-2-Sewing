74147f290725987f1e1a8af43417f75ee83c3b35
junse02 <67049713+junseo021014@users.noreply.github.com>
2026-05-25 22:24:01 +0900

    🔨 fix: 응답 개체 Setter 추가

--- src/main/java/com/mediator/cider/domain/mediation/dto/ai/CycleExploreResponse.java
+++ src/main/java/com/mediator/cider/domain/mediation/dto/ai/CycleExploreResponse.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CycleExploreResponse {
